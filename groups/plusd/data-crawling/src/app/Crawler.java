013887d2a60d69f91f119be15733c3c15bfa20a4
Frederic Jan Ouwehand <fredericjan.ouwehand@epfl.ch>
2015-03-15 18:15:08 +0100

    From and To dates from arguments/default #4

--- groups/plusd/data-crawling/src/app/Crawler.java
+++ groups/plusd/data-crawling/src/app/Crawler.java
@@ -2,6 +2,9 @@ package app;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
@@ -19,6 +22,11 @@ import org.apache.hadoop.util.ToolRunner;
 public class Crawler extends Configured implements Tool {
 
 	public static Path datesInputPath = new Path("input-dates.txt");
+
+	// Minimum/Maximum plusD available date
+	// https://www.wikileaks.org/plusd/
+	public static String defaultFrom = "1966-01-01";
+	public static String defaultTo = "2010-12-31";
 
 	public static enum Errors {
 		NONE(0), BAD_NB_ARGS(1), INTERNAL_UNEXPECTED_CASE(2), JOB_COMPLETION(3), INPUT_FILE_WRITE(
@@ -58,13 +66,13 @@ public class Crawler extends Configured implements Tool {
 		}
 	}
 
-	public static boolean writeInputFile(FileSystem fs, Path path)
-			throws IOException {
+	public static boolean writeInputFile(FileSystem fs, Path path,
+			Date fromDate, Date toDate) throws IOException {
 		OutputStreamWriter osw = new OutputStreamWriter(fs.create(path));
 
 		StringBuilder dates = new StringBuilder();
 
-		// TODO: fill dates with the actual dates
+		// TODO: fill dates from the (from, to) dates
 
 		osw.write(dates.toString());
 		osw.flush();
@@ -81,44 +89,61 @@ public class Crawler extends Configured implements Tool {
 	public int run(String[] args) throws Exception {
 		int returnCode = Errors.UNASSIGNED_ERROR_CODE.getValue();
 
+		// Getting the input (from, to) dates
+		SimpleDateFormat defaultDF = new SimpleDateFormat("yyyy-MM-dd");
+		Date fromDate = defaultDF.parse(defaultFrom);
+		Date toDate = defaultDF.parse(defaultTo);
 		if (args.length != 2) {
-			System.out.println("Arguments: <from-year> <to-year>");
-			System.out.println("Example: 1973 2010");
-
-			returnCode = Errors.BAD_NB_ARGS.getValue();
+			System.out
+					.println("N.B. Possible arguments are: <from-year> <to-year>");
+			System.out.println(String.format("-> Using defaults: %s to %s",
+					defaultFrom, defaultTo));
 		} else {
-			Configuration conf = this.getConf();
-			Job job = Job.getInstance(conf, "Wikileaks - PlusD data Crawler");
-			FileSystem fs = FileSystem.get(conf);
-
-			job.setJarByClass(Crawler.class);
-			job.setMapperClass(CrawlerMapper.class);
-			job.setReducerClass(CrawlerReducer.class);
-			job.setOutputKeyClass(Text.class);
-			job.setOutputValueClass(Text.class);
-
-			job.setInputFormatClass(TextInputFormat.class);
-			job.setOutputFormatClass(NullOutputFormat.class);
-			TextInputFormat.setMaxInputSplitSize(job, Long.MAX_VALUE);
-
-			// TODO: extract from-to dates as arguments
-
-			// Create & add to program dateInputFile
-			if (writeInputFile(fs, datesInputPath)) {
-				TextInputFormat.addInputPath(job, datesInputPath);
-
-				// Run the job
-				if (job.waitForCompletion(true)) {
-					returnCode = Errors.NONE.getValue();
-				} else {
-					returnCode = Errors.JOB_COMPLETION.getValue();
-				}
-
-				// Do the cleaning
-				fs.delete(datesInputPath, true);
-			} else {
-				returnCode = Errors.INPUT_FILE_WRITE.getValue();
+			String fromInputStr = args[0];
+			String toInputStr = args[1];
+			try {
+				fromDate = defaultDF.parse(fromInputStr);
+				toDate = defaultDF.parse(toInputStr);
+			} catch (ParseException pe) {
+				System.out.println(String.format(
+						"Failed to parse input dates %s and %s", fromInputStr,
+						toInputStr));
+				System.out.println(String.format("-> Using defaults: %s to %s",
+						defaultFrom, defaultTo));
 			}
+		}
+
+		Configuration conf = this.getConf();
+		Job job = Job.getInstance(conf, "Wikileaks - PlusD data Crawler");
+		FileSystem fs = FileSystem.get(conf);
+
+		job.setJarByClass(Crawler.class);
+		job.setMapperClass(CrawlerMapper.class);
+		job.setReducerClass(CrawlerReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(NullOutputFormat.class);
+		TextInputFormat.setMaxInputSplitSize(job, Long.MAX_VALUE);
+
+		// TODO: extract from-to dates as arguments
+
+		// Create & add to program dateInputFile
+		if (writeInputFile(fs, datesInputPath, fromDate, toDate)) {
+			TextInputFormat.addInputPath(job, datesInputPath);
+
+			// Run the job
+			if (job.waitForCompletion(true)) {
+				returnCode = Errors.NONE.getValue();
+			} else {
+				returnCode = Errors.JOB_COMPLETION.getValue();
+			}
+
+			// Do the cleaning
+			fs.delete(datesInputPath, true);
+		} else {
+			returnCode = Errors.INPUT_FILE_WRITE.getValue();
 		}
 
 		return returnCode;
