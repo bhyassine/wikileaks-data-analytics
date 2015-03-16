162cffc36a5c1053df3ab66eaceaedd5cad0a9b3
Frederic Jan Ouwehand <fredericjan.ouwehand@epfl.ch>
2015-03-16 17:40:01 +0100

    Implement a web-page fetcher

--- groups/plusd/data-crawling/src/utils/PageLoader.java
+++ groups/plusd/data-crawling/src/utils/PageLoader.java
@@ -2,12 +2,17 @@ package utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.zip.GZIPOutputStream;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 public class PageLoader {
 
@@ -21,6 +26,37 @@ public class PageLoader {
 		qBF.close();
 
 		return content;
+	}
+
+	public static boolean getAndSave(FileSystem fs, String dst, String urlStr,
+			boolean gzCompression) {
+		boolean sucess = false;
+
+		HttpURLConnection connection;
+		try {
+			connection = (HttpURLConnection) (new URL(urlStr)).openConnection();
+			InputStream pageIS = connection.getInputStream();
+			OutputStream outputOS = fs.create(new Path(dst));
+
+			// Save the page
+			if (gzCompression) {
+				GZIPOutputStream outputGIS = new GZIPOutputStream(outputOS);
+				org.apache.hadoop.io.IOUtils.copyBytes(pageIS, outputGIS,
+						fs.getConf());
+				outputGIS.close();
+			} else {
+				org.apache.hadoop.io.IOUtils.copyBytes(pageIS, outputOS,
+						fs.getConf());
+			}
+
+			outputOS.close();
+			pageIS.close();
+			sucess = true;
+		} catch (IOException e) {
+			sucess = false;
+		}
+
+		return sucess;
 	}
 
 }
