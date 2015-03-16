8fc04055c512042b577dbd168f1c85fa4290a340
Frederic Jan Ouwehand <fredericjan.ouwehand@epfl.ch>
2015-03-16 17:41:31 +0100

    Saving wikileaks cables is OK close #4

--- groups/plusd/data-crawling/src/app/CrawlerReducer.java
+++ groups/plusd/data-crawling/src/app/CrawlerReducer.java
@@ -2,9 +2,11 @@ package app;
 
 import java.io.IOException;
 
-import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import utils.PageLoader;
 
 class CrawlerReducer extends Reducer<Text, Text, Text, Text> {
 
@@ -16,17 +18,31 @@ class CrawlerReducer extends Reducer<Text, Text, Text, Text> {
 			StringBuilder errorMsg = new StringBuilder();
 			for (Text e : occurences) {
 				errorMsg.append(Crawler.newline);
-				errorMsg.append("ERROR: ");
+				errorMsg.append(Crawler.errorToken);
+				errorMsg.append(Crawler.separator);
 				errorMsg.append(e.toString());
 			}
-			context.write(new Text("ERRORS_BELOW"),
-					new Text(errorMsg.toString()));
+			context.write(new Text(String.valueOf(Crawler.newline)), new Text(
+					errorMsg.toString()));
 		} else {
 			// Download and save the page
-			Configuration conf = context.getConfiguration();
-			context.write(new Text("SAVED"), refid);
+			FileSystem fs = FileSystem.get(context.getConfiguration());
 
-			// TODO: download the page
+			String fileName = refid.toString();
+			String outputFolder = Crawler.pagesSavingLocation;
+			String extension = Crawler.gzCompressionInOutput ? "gz" : "html";
+			String outputPath = outputFolder + "/" + fileName + "." + extension;
+			String urlStr = Crawler.downloaderURL + "/" + fileName + ".html";
+
+			if (PageLoader.getAndSave(fs, outputPath, urlStr,
+					Crawler.gzCompressionInOutput)) {
+				context.write(new Text("SAVED"), refid);
+			} else {
+				context.write(
+						new Text(Crawler.errorToken),
+						new Text(String.format("Cannot get&save page %s",
+								outputPath)));
+			}
 		}
 	}
 }
