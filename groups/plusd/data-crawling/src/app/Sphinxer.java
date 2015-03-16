04b52e566fb801ec3429461f41eaf11ab54d8151
Frederic Jan Ouwehand <fredericjan.ouwehand@epfl.ch>
2015-03-16 09:48:58 +0100

    Begin askForRefIDListing

--- groups/plusd/data-crawling/src/app/Sphinxer.java
+++ groups/plusd/data-crawling/src/app/Sphinxer.java
@@ -1,7 +1,7 @@
 package app;
 
-import java.util.AbstractMap;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
+import java.util.Stack;
 
 import org.codehaus.jettison.json.JSONObject;
 
@@ -15,13 +15,13 @@ import org.codehaus.jettison.json.JSONObject;
  * 	project			[mandatory]	The associated dataset
  * 	qcanonical		[optional]	(?)
  * 	qcanonical_seal	[mandatory]	(?) For each query rights-level, a security seal is associated
- * 	qtfrom			[???]		From date (timestamp)
- * 	qtto			[???]		To date (timestamp)
+ * 	qtfrom			[optional]	From date (timestamp)
+ * 	qtto			[optional]	To date (timestamp)
  * 	tkey_from		[optional]	From date (timestamp)
  * 	tkey_to			[optional]	To date (timestamp)
- * 	qsort			[???]		results sorting type
- * 	qlimit			[???]		(?) cardinality of results
- * 	token			[???]		(?) The FROM of a LIMIT statement
+ * 	qsort			[optional]	results sorting type
+ * 	qlimit			[optional]	Cardinality of results. Min = 20, Max = 500
+ * 	token			[mandatory]	! The document ID [1, 2325959] !
  * 	unit			[optional]	The level of aggregation
  */
 public class Sphinxer {
@@ -47,14 +47,14 @@ public class Sphinxer {
 				"doc_list_from_query");
 
 		private String value;
+		public static String key = "command";
 
 		private Cmds(String value) {
 			this.value = value;
 		}
 
-		public Map.Entry<String, String> getValue() {
-			return new AbstractMap.SimpleEntry<String, String>("command",
-					this.value);
+		public String getValue() {
+			return this.value;
 		}
 	}
 
@@ -62,14 +62,14 @@ public class Sphinxer {
 		ALL_CABLES("all_cables");
 
 		private String value;
+		public static String key = "project";
 
 		private Projects(String value) {
 			this.value = value;
 		}
 
-		public Map.Entry<String, String> getValue() {
-			return new AbstractMap.SimpleEntry<String, String>("project",
-					this.value);
+		public String getValue() {
+			return this.value;
 		}
 	}
 
@@ -77,14 +77,14 @@ public class Sphinxer {
 		PUBLIC_READ("7fa94db3387685fe93c1c13cdca27a62");
 
 		private String value;
+		public static String key = "qcanonical_seal";
 
 		private CanonicalSeals(String value) {
 			this.value = value;
 		}
 
-		public Map.Entry<String, String> getValue() {
-			return new AbstractMap.SimpleEntry<String, String>(
-					"qcanonical_seal", this.value);
+		public String getValue() {
+			return this.value;
 		}
 	}
 
@@ -92,15 +92,38 @@ public class Sphinxer {
 		DAY("day"), MONTH("month");
 
 		private String value;
+		public static String key = "unit";
 
 		private Units(String value) {
 			this.value = value;
 		}
 
-		public Map.Entry<String, String> getValue() {
-			return new AbstractMap.SimpleEntry<String, String>("unit",
-					this.value);
+		public String getValue() {
+			return this.value;
 		}
+	}
+
+	public static enum Sorts {
+		TIME_ASC("tasc");
+
+		private String value;
+		public static String key = "qsort";
+
+		private Sorts(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return this.value;
+		}
+	}
+
+	public static class QLimit {
+		public static String key = "qlimit";
+	}
+
+	public static class Token {
+		public static String key = "token";
 	}
 
 	/**
@@ -108,19 +131,22 @@ public class Sphinxer {
 	 * on the sphinxer cmds.
 	 * 
 	 * e.g. sphinxerCmds.STATS_FROM_QUERY only accepts sphinxerFormat.JSON
+	 * 
+	 * Note that html provides less information in some cases and hence
+	 * gives a smaller response.
 	 */
 	public static enum Format {
 		JSON("json"), HTML("html");
 
 		private String value;
+		public static String key = "format";
 
 		private Format(String value) {
 			this.value = value;
 		}
 
-		public Map.Entry<String, String> getValue() {
-			return new AbstractMap.SimpleEntry<String, String>("format",
-					this.value);
+		public String getValue() {
+			return this.value;
 		}
 	}
 
@@ -142,17 +168,48 @@ public class Sphinxer {
 	}
 
 	/**
-	 * Ask Sphinxer for documents meta listing.
+	 * Ask Sphinxer for documents ref_id listing.
 	 * Can take these parameters:
-	 *	(?)
+	 *	token, qlimit, qsort, (qtto), (qtfrom),
+	 *	qcanonical_seal, project, command, format
 	 * 
 	 * @return the JSON query result
+	 * @throws UnsupportedEncodingException 
 	 */
-	public static JSONObject askForMetaListing() {
-		JSONObject jsonRes = new JSONObject();
+	public static Stack<String> askForRefIDListing(int fromDocumentNo,
+			int nbOfDocuments) throws UnsupportedEncodingException,
+			IllegalArgumentException {
+		Stack<String> refIdStack = new Stack<String>();
 
-		// TODO: implement
+		int qlimit = nbOfDocuments;
+		qlimit = Math.min(qlimit, 500);
+		qlimit = Math.max(qlimit, 20);
+		if (qlimit != nbOfDocuments) {
+			throw new IllegalArgumentException(
+					"nbOfDocuments should be between 20 and 500");
+		}
 
-		return jsonRes;
+		StringBuilder URLStr = new StringBuilder();
+		URLStr.append(Sphinxer.availableProtocols.HTTP.getValue());
+		URLStr.append(Sphinxer.urlStr);
+
+		QueryString parameters = new QueryString();
+		parameters.add(Token.key, String.valueOf(fromDocumentNo));
+		parameters.add(QLimit.key, String.valueOf(qlimit));
+		parameters.add(Sphinxer.Sorts.key, Sphinxer.Sorts.TIME_ASC.getValue());
+		parameters.add(Sphinxer.CanonicalSeals.key,
+				Sphinxer.CanonicalSeals.PUBLIC_READ.getValue());
+		parameters.add(Sphinxer.Projects.key,
+				Sphinxer.Projects.ALL_CABLES.getValue());
+		parameters.add(Sphinxer.Cmds.key,
+				Sphinxer.Cmds.DOC_LIST_FROM_QUERY.getValue());
+		parameters.add(Sphinxer.Format.key, Sphinxer.Format.HTML.getValue());
+
+		URLStr.append(parameters.toString());
+
+		// TODO: Fetch URLStr
+		// TODO: Extract ref_ids from it
+
+		return refIdStack;
 	}
 }
