862385e6d2fd08aa1d8745ee592c8b8da05dc68f
Frederic Jan Ouwehand <fredericjan.ouwehand@epfl.ch>
2015-03-16 17:00:53 +0100

    Manage the error case #4

--- groups/plusd/data-crawling/src/app/CrawlerReducer.java
+++ groups/plusd/data-crawling/src/app/CrawlerReducer.java
@@ -8,14 +8,25 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 class CrawlerReducer extends Reducer<Text, Text, Text, Text> {
 
-	public void reduce(Text refid, Iterable<Text> dontCare, Context context)
+	public void reduce(Text refid, Iterable<Text> occurences, Context context)
 			throws IOException, InterruptedException {
 
-		Configuration conf = context.getConfiguration();
+		if (refid.toString().equals(Crawler.errorToken)) {
+			// We have to report the errors
+			StringBuilder errorMsg = new StringBuilder();
+			for (Text e : occurences) {
+				errorMsg.append(Crawler.newline);
+				errorMsg.append("ERROR: ");
+				errorMsg.append(e.toString());
+			}
+			context.write(new Text("ERRORS_BELOW"),
+					new Text(errorMsg.toString()));
+		} else {
+			// Download and save the page
+			Configuration conf = context.getConfiguration();
+			context.write(new Text("SAVED"), refid);
 
-		// TODO: do something with the html file corresponding to its
-		// pair.page_id
-		// TODO: handle case with refid = errorToken
-		context.write(refid, new Text("downloaded"));
+			// TODO: download the page
+		}
 	}
 }
