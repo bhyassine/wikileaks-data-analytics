231deaa69c95d7af90a8e51af3d1623ff787b646
Florian Briant <florian.briant@epfl.ch>
2015-03-18 11:02:36 +0100

    Setting up working environment for HTML parsing.

---
+++ groups/plusd/data-cleaning/src/WikiLeakHTMLParser.java
@@ -0,0 +1,10 @@
+
+public class WikiLeakHTMLParser {
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Bad number of arguments. Expected use : <program> <directory of HTML files> <output directory>");
+			System.exit(1);
+		}
+		System.out.println("Hello Flo");
+	}
+}
